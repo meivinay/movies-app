26588f8eb2b0c0af10a3879710d299ef37e36c46
Vinay <vinay.t@collegedunia.com>
2024-10-15 06:41:00 +0530

    remove key

--- src/apikey.js
+++ src/apikey.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://api.themoviedb.org/3";
-export const API_KEY = "7cab9ea3170bca62eb51763f7f06c818";
+export const API_KEY = "";
 export const POSTER_URL = "https://image.tmdb.org/t/p/original";   
